d2b53054dd7954273ba82e76301bfe25e036bc82
John Malloc <falsinimassimiliano@gmail.com>
2024-09-07 12:50:20 +0200

    feat: get file names

---
+++ src/files.rs
@@ -0,0 +1,69 @@
+/**
+ * frontend-minify reduce the size of the files of frontend applications
+ * Copyright (C) 2024 john-malloc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+fn cout_files() -> usize {
+    let mut i: usize = 0;
+    for element in walkdir::WalkDir::new(".") {
+        let entry = match element {
+            Ok(e) => e,
+            Err(err) => panic!("Failed on reading files -> {}", err),
+        };
+        let file_name = entry
+            .path()
+            .file_name()
+            .and_then(std::ffi::OsStr::to_str)
+            .unwrap_or("");
+        if !file_name.contains(".")
+            || file_name.ends_with("min.html")
+            || file_name.ends_with("min.css")
+            || file_name.ends_with("min.js")
+        {
+            continue;
+        }
+        if file_name.ends_with("html") || file_name.ends_with("css") || file_name.ends_with("js") {
+            i += 1;
+        }
+    }
+    return i;
+}
+
+pub fn get_files() -> Vec<String> {
+    let mut vec = vec![String::new(); cout_files()];
+    for element in walkdir::WalkDir::new(".") {
+        let entry = match element {
+            Ok(e) => e,
+            Err(err) => panic!("Failed on reading files -> {}", err),
+        };
+        let file_name = entry
+            .path()
+            .file_name()
+            .and_then(std::ffi::OsStr::to_str)
+            .unwrap_or("")
+            .to_owned();
+        if file_name.ends_with("min.html")
+            || file_name.ends_with("min.css")
+            || file_name.ends_with("min.js")
+            || !file_name.contains(".")
+        {
+            continue;
+        }
+        if file_name.ends_with("html") || file_name.ends_with("css") || file_name.ends_with("js") {
+            vec.push(file_name);
+        }
+    }
+    return vec;
+}
